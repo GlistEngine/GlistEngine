8fd681ebf64071babd6fc63a2b5dca432e792fe9
emrullah-sahin <emrullahsahin551@gmail.com>
2020-12-11 11:57:49 +0300

    Fixed doxygen.

--- engine/graphics/gRect.cpp
+++ engine/graphics/gRect.cpp
@@ -10,123 +10,60 @@
 /**
  * Default constructor sets values to zero.
  */
-gRect::gRect() : left(0), top(0), right(0), bottom(0) {}
+gRect::gRect() : l(0), t(0), r(0), b(0), width(0), height(0) {}
 
-gRect::gRect(int left, int top, int right, int bottom) : left(left), top(top), right(right), bottom(bottom) {}
+gRect::gRect(int left, int top, int right, int bottom) : l(left), t(top), r(right), b(bottom), width(right - left), height(bottom - top) {}
 
-gRect::gRect(const gRect& r) : left(r.left), top(r.top), right(r.right), bottom(r.bottom) {}
+gRect::gRect(const gRect& r) : l(r.left()), t(r.top()), r(r.right()), b(r.bottom()), width(r.right() - r.left()), height(r.bottom() - r.top()) {}
 
 gRect::~gRect() {}
 
-/**
- * Sets edges of the rectangle.
- * @param left left edge to be set
- * @param top top edge to be set
- * @param right right edge to be set
- * @param bottom bottom edge to be set
- */
 void gRect::set(int left, int top, int right, int bottom)
 {
-	this->left = left;
-	this->top = top;
-	this->right = right;
-	this->bottom = bottom;
+	l = left;
+	t = top;
+	r = right;
+	b = bottom;
+	width = right - left;
+	height = bottom - top;
 }
 
-/**
- * Sets edges of the rectangle with the given rectangle
- * @param r Rectangle to be set
- */
 void gRect::set(const gRect& r)
 {
-	left = r.left;
-	top = r.top;
-	right = r.right;
-	bottom = r.bottom;
+	l = r.left();
+	t = r.top();
+	this->r = r.right();
+	b = r.bottom();
+	width = r.getWidth();
+	height = r.getHeight();
 }
 
-/**
- * Checks whether of intersection given 2 rectangle.
- * @param left1 left edge of the first rectangle
- * @param top1 top edge of the first rectangle
- * @param right1 right edge of the first rectangle
- * @param bottom1 bottom edge of the first rectangle
- * @param left2 left edge of the second rectangle
- * @param top2 top edge of the second rectangle
- * @param right2 right edge of the second rectangle
- * @param bottom2 bottom edge of the second rectangle
- * @return true if there is intersection false if not.
- */
 bool gRect::intersects(int left1, int top1, int right1, int bottom1, int left2, int top2, int right2, int bottom2) { return left1 < right2 && right1 > left2 && top1 < bottom2 && bottom1 > top2; }
 
-/**
- * Checks whether of intersection given 2 rectangle.
- * @param r1 first rectangle to be check.
- * @param r2 second rectangle to be check.
- * @return true if there is intersection false if not.
- */
-bool gRect::intersects(const gRect& r1, const gRect& r2) { return intersects(r1.left, r1.top, r1.right, r1.bottom, r2.left, r2.top, r2.right, r2.bottom); }
+bool gRect::intersects(const gRect& r1, const gRect& r2) { return intersects(r1.left(), r1.top(), r1.right(), r1.bottom(), r2.left(), r2.top(), r2.right(), r2.bottom()); }
 
-/**
- * Checks whether of intersection with given rectangle.
- * @param left left edge of the given rectangle
- * @param top top edge of the given rectangle
- * @param right right edge of the given rectangle
- * @param bottom bottom edge of the given rectangle
- * @return true if there is intersection false if not.
- */
-bool gRect::intersects(int left, int top, int right, int bottom) { return intersects(this->left, this->top, this->right, this->bottom, left, top, right, bottom); }
+bool gRect::intersects(int left, int top, int right, int bottom) { return intersects(l, t, r, b, left, top, right, bottom); }
 
-/**
- * Checks whether of intersection with given rectangle.
- * @param r rectangle to be check.
- * @return true if there is intersection false if not.
- */
-bool gRect::intersects(const gRect& r) { return intersects(r.left, r.top, r.right, r.bottom); }
+bool gRect::intersects(const gRect& r) { return intersects(r.left(), r.top(), r.right(), r.bottom()); }
 
-/**
- * Checks if second rectangles edges are inside or equal of the first rectangle.
- * @param left1 left edge of the first rectangle
- * @param top1 top edge of the first rectangle
- * @param right1 right edge of the first rectangle
- * @param bottom1 bottom edge of the first rectangle
- * @param left2 left edge of the second rectangle
- * @param top2 top edge of the second rectangle
- * @param right2 right edge of the second rectangle
- * @param bottom2 bottom edge of the second rectangle
- * @return true if second rectangle is inside of first rectangle false if not.
- */
 bool gRect::contains(int left1, int top1, int right1, int bottom1, int left2, int top2, int right2, int bottom2) { return left1 <= left2 && right1 >= right2 && top1 <= top2 && bottom1 >= bottom2; }
 
-/**
- * Checks if second rectangles edges are inside or equal of the first rectangle.
- * @param r1 first rectangle to be check.
- * @param r2 second rectangle to be check.
- * @return true if second rectangle is inside of first rectangle false if not.
- */
-bool gRect::contains(const gRect& r1, const gRect& r2) { return contains(r1.left, r1.top, r1.right, r1.bottom, r2.left, r2.top, r2.right, r2.bottom); }
+bool gRect::contains(const gRect& r1, const gRect& r2) { return contains(r1.left(), r1.top(), r1.right(), r1.bottom(), r2.left(), r2.top(), r2.right(), r2.bottom()); }
 
-/**
- * Checks if given rectangles edges are inside or equal of the current rectangle.
- * @param left left edge of the given rectangle
- * @param top top edge of the given rectangle
- * @param right right edge of the given rectangle
- * @param bottom bottom edge of the given rectangle
- * @return true if given rectangle is inside of current rectangle false if not.
- */
-bool gRect::contains(int left, int top, int right, int bottom) { return contains(this->left, this->top, this->right, this->bottom, left, top, right, bottom); }
+bool gRect::contains(int left, int top, int right, int bottom) { return contains(l, t, r, b, left, top, right, bottom); }
 
-/**
- * Checks if given rectangles edges are inside or equal of the current rectangle.
- * @param r rectangle to be check.
- * @return true if given rectangle is inside of current rectangle false if not.
- */
-bool gRect::contains(const gRect& r) { return contains(r.left, r.top, r.right, r.bottom); }
+bool gRect::contains(const gRect& r) { return contains(r.left(), r.top(), r.right(), r.bottom()); }
 
-/**
- * Checks if given point are inside of the current rectangle.
- * @param x X Position of the given point
- * @param y Y Position of the given point
- * @return true if given point is inside of current rectangle false if not.
- */
 bool gRect::contains(int x, int y) { return contains(x, y, x, y); }
+
+int gRect::left() 	const { return l; }
+
+int gRect::top()	const { return t; }
+
+int gRect::right()	const { return r; }
+
+int gRect::bottom()	const { return b; }
+
+int gRect::getWidth()  const { return width; }
+
+int gRect::getHeight() const { return height; }
--- engine/graphics/gRect.h
+++ engine/graphics/gRect.h
@@ -1,37 +1,174 @@
 /*
- * gRect.h
+ * Copyright (C) 2016 Nitra Games Ltd.
  *
- *  Created on: 9 Ara 2020
- *      Author: Emrullah
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #ifndef GRAPHICS_GRECT_H_
 #define GRAPHICS_GRECT_H_
-
+/**
+ * gRect holds six integer for a rectangle. The rectangle is represented by the coordinates of its four edges(left, top, right, bottom).
+ * These fields can be accessed via their respective functions(e.g. to acces left edge of the rectangle use left()).
+ * Use getWidth() and getHeight() to retrieve the rectangles width and height.
+ * To set edges of the rectangle use one of the set function.
+ */
 class gRect {
-public:
-	int left, right, top, bottom;
+	private:
+	int l, r, t, b;
+	int width, height;
+	public:
+	/**
+	 * Creates empty rectangle
+	 */
 	gRect();
+	/**
+	 * Creates rectangle with given edges.
+	 * @param left 	 Left edge of the rectangle to be created.
+	 * @param top 	 Top edge of the rectangle to be created.
+	 * @param right  Right edge of the rectangle to be created.
+	 * @param bottom Bottom edge of the rectangle to be created.
+	 */
 	gRect(int left, int top, int right, int bottom);
+	/**
+	 * Creates copy of the given rectangle.
+	 * @param r Rectangle to be copied.
+	 */
 	gRect(const gRect& r);
 	virtual ~gRect();
 
+	/**
+	 * Sets edges of the rectangle.
+	 * @param left 	 Left edge to be set
+	 * @param top 	 Top edge to be set
+	 * @param right  Right edge to be set
+	 * @param bottom Bottom edge to be set
+	 */
 	void set(int left, int top, int right, int bottom);
+	/**
+	 * Sets edges of the rectangle with the given rectangle
+	 * @param r Rectangle to be set
+	 */
 	void set(const gRect& r);
 
+	/**
+	 * Checks whether of intersection given 2 rectangle.
+	 * @param left1   Left edge of the first rectangle
+	 * @param top1 	  Top edge of the first rectangle
+	 * @param right1  Right edge of the first rectangle
+	 * @param bottom1 Bottom edge of the first rectangle
+	 * @param left2   Left edge of the second rectangle
+	 * @param top2 	  Top edge of the second rectangle
+	 * @param right2  Right edge of the second rectangle
+	 * @param bottom2 Bottom edge of the second rectangle
+	 * @return true if there is intersection false if not.
+	 */
 	static bool intersects(int left1, int top1, int right1, int bottom1, int left2, int top2, int right2, int bottom2);
+	/**
+	 * Checks whether of intersection given 2 rectangle.
+	 * @param r1 First rectangle to be check.
+	 * @param r2 Second rectangle to be check.
+	 * @return true if there is intersection false if not.
+	 */
 	static bool intersects(const gRect& r1, const gRect& r2);
+	/**
+	 * Checks whether of intersection with given rectangle.
+	 * @param left 	 Left edge of the given rectangle
+	 * @param top 	 Top edge of the given rectangle
+	 * @param right  Right edge of the given rectangle
+	 * @param bottom Bottom edge of the given rectangle
+	 * @return true if there is intersection false if not.
+	 */
 	bool intersects(int left, int top, int right, int bottom);
+	/**
+	 * Checks whether of intersection with given rectangle.
+	 * @param r Rectangle to be check.
+	 * @return true if there is intersection false if not.
+	 */
 	bool intersects(const gRect& r);
 
+	/**
+	 * Checks if second rectangles edges are inside or equal of the first rectangle.
+	 * @param left1   Left edge of the first rectangle
+	 * @param top1 	  Top edge of the first rectangle
+	 * @param right1  Right edge of the first rectangle
+	 * @param bottom1 Bottom edge of the first rectangle
+	 * @param left2   Left edge of the second rectangle
+	 * @param top2 	  Top edge of the second rectangle
+	 * @param right2  Right edge of the second rectangle
+	 * @param bottom2 Bottom edge of the second rectangle
+	 * @return true if second rectangle is inside of first rectangle false if not.
+	 */
 	static bool contains(int left1, int top1, int right1, int bottom1, int left2, int top2, int right2, int bottom2);
+	/**
+	 * Checks if second rectangles edges are inside or equal of the first rectangle.
+	 * @param r1 First rectangle to be check.
+	 * @param r2 Second rectangle to be check.
+	 * @return true if second rectangle is inside of first rectangle false if not.
+	 */
 	static bool contains(const gRect& r1, const gRect& r2);
+	/**
+	 * Checks if given rectangles edges are inside or equal of the current rectangle.
+	 * @param left 	 Left edge of the given rectangle
+	 * @param top 	 Top edge of the given rectangle
+	 * @param right  Right edge of the given rectangle
+	 * @param bottom Bottom edge of the given rectangle
+	 * @return true if given rectangle is inside of current rectangle false if not.
+	 */
 	bool contains(int left, int top, int right, int bottom);
+	/**
+	 * Checks if given rectangles edges are inside or equal of the current rectangle.
+	 * @param r Rectangle to be check.
+	 * @return true if given rectangle is inside of current rectangle false if not.
+	 */
 	bool contains(const gRect& r);
+	/**
+	 * Checks if given point are inside of the current rectangle.
+	 * @param x X Position of the given point
+	 * @param y Y Position of the given point
+	 * @return true if given point is inside of current rectangle false if not.
+	 */
 	bool contains(int x, int y);
 
-	inline int getWidth()  const { return right - left; }
-	inline int getHeight() const { return bottom - top; }
+	/**
+	 * Returns left edge of the rectangle.
+	 * @return Left edge of the rectangle.
+	 */
+	int left() const;
+	/**
+	 * Returns top edge of the rectangle.
+	 * @return Top edge of the rectangle.
+	 */
+	int top() const;
+	/**
+	 * Returns right edge of the rectangle.
+	 * @return Right edge of the rectangle.
+	 */
+	int right() const;
+	/**
+	 * Returns bottom edge of the rectangle.
+	 * @return Bottom edge of the rectangle.
+	 */
+	int bottom() const;
+	/**
+	 * Returns width of the rectangle.
+	 * @return Width of the rectangle.
+	 */
+	int getWidth()  const;
+	/**
+	 * Returns height of the rectangle.
+	 * @return Height of the rectangle.
+	 */
+	int getHeight() const;
 };
 
 #endif /* GRAPHICS_GRECT_H_ */
