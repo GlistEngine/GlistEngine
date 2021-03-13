29a3a39ec39c8e3427cd23220ae4fefb6cb9cdd7
R.Noyan Culum <noyanculum@yahoo.com>
2021-03-13 17:39:53 +0300

    Fixed Linux launch

--- engine/core/gGLFWWindow.cpp
+++ engine/core/gGLFWWindow.cpp
@@ -76,11 +76,13 @@ void gGLFWWindow::initialize(int width, int height, int windowMode) {
 	    }
 	}
 
+#ifndef LINUX
 	GLFWimage images[1];
 	std::string iconpath = gGetImagesDir() + "gameicon/icon.png";
 	images[0].pixels = stbi_load(iconpath.c_str(), &images[0].width, &images[0].height, 0, 4); //rgba channels
 	glfwSetWindowIcon(window, 1, images);
 	stbi_image_free(images[0].pixels);
+#endif
 
 	glfwMakeContextCurrent(window);
 	glewExperimental = GL_TRUE;
