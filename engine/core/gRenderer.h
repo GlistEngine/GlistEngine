895927fcb9297715867fed4a4630d25f9c223f8c
mberkayersoy <76611569+mberkayersoy@users.noreply.github.com>
2021-08-02 01:50:52 +0300

    Update gRenderer.h

--- engine/core/gRenderer.h
+++ engine/core/gRenderer.h
@@ -45,6 +45,7 @@ int gGetCullingDirection();
 
 void gDrawLine(float x1, float y1, float x2, float y2);
 void gDrawLine(float x1, float y1, float z1, float x2, float y2, float z2);
+void gDrawArrow(float x1, float y1, float x2, float y2, float tipLength, float tipDegree);
 
 
 class gRenderer: public gObject {
