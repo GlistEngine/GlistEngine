f99017113d2796c14471b7be27b09f7d1ade40f6
kayraurfali <kayra.99@hotmail.com>
2021-11-23 13:10:31 +0300

    Update gKeyCode.h

--- engine/utils/gKeyCode.h
+++ engine/utils/gKeyCode.h
@@ -2,69 +2,130 @@
  * gKeyCode.h
  *
  *  Created on: 8 Aðu 2021
- *      Author: kayra
+ *  Author: kayra
  */
 
 #ifndef UTILS_GKEYCODE_H_
 #define UTILS_GKEYCODE_H_
 
-enum class gKeyCode {
-	NONE = 0,
-	KEY_0 = 48,
-	KEY_1 = 49,
-	KEY_2 = 50,
-	KEY_3 = 51,
-	KEY_4 = 52,
-	KEY_5 = 53,
-	KEY_6 = 54,
-	KEY_7 = 55,
-	KEY_8 = 56,
-	KEY_9 = 57,
-	A = 65,
-	B = 66,
-	C = 67,
-	D = 68,
-	E = 69,
-	F = 70,
-	G = 71,
-	H = 72,
-	I = 73,
-	J = 74,
-	K = 75,
-	L = 76,
-	M = 77,
-	N = 78,
-	O = 79,
-	P = 80,
-	Q = 81,
-	R = 82,
-	S = 83,
-	T = 84,
-	U = 85,
-	V = 86,
-	W = 87,
-	X = 88,
-	Y = 89,
-	Z = 90,
-	ESC = 256,
-	TAB = 258,
-	CAPS = 280,
-	NUMPAD_0 = 320,
-	NUMPAD_1 = 321,
-	NUMPAD_2 = 322,
-	NUMPAD_3 = 323,
-	NUMPAD_4 = 324,
-	NUMPAD_5 = 325,
-	NUMPAD_6 = 326,
-	NUMPAD_7 = 327,
-	NUMPAD_8 = 328,
-	NUMPAD_9 = 329,
-	LEFT_SHIFT = 340,
-	LEFT_CTRL = 341,
-	ALT = 342,
-	RIGHT_SHIFT = 344,
-	RIGHT_CTRL = 345,
-	ALTGR = 346
-};
+#define G_KEY_NONE 0;
+/* numeric */
+#define G_KEY_0 48;
+#define G_KEY_1 49;
+#define G_KEY_2 50;
+#define G_KEY_3 51;
+#define G_KEY_4 52;
+#define G_KEY_5 53;
+#define G_KEY_6 54;
+#define G_KEY_7 55;
+#define G_KEY_8 56;
+#define G_KEY_9 57;
+
+/* alphabetic */
+#define G_KEY_A 65;
+#define G_KEY_B 66;
+#define G_KEY_C 67;
+#define G_KEY_D 68;
+#define G_KEY_E 69;
+#define G_KEY_F 70;
+#define G_KEY_G 71;
+#define G_KEY_H 72;
+#define G_KEY_I 73;
+#define G_KEY_J 74;
+#define G_KEY_K 75;
+#define G_KEY_L 76;
+#define G_KEY_M 77;
+#define G_KEY_N 78;
+#define G_KEY_O 79;
+#define G_KEY_P 80;
+#define G_KEY_Q 81;
+#define G_KEY_R 82;
+#define G_KEY_S 83;
+#define G_KEY_T 84;
+#define G_KEY_U 85;
+#define G_KEY_V 86;
+#define G_KEY_W 87;
+#define G_KEY_X 88;
+#define G_KEY_Y 89;
+#define G_KEY_Z 90;
+
+/* functional */
+#define G_KEY_ESC 256;
+#define G_KEY_ENTER 257;
+#define G_KEY_ENTER 257
+#define G_KEY_TAB 258
+#define G_KEY_BACKSPACE 259
+#define G_KEY_INSERT 260
+#define G_KEY_DELETE 261
+#define G_KEY_RIGHT 262
+#define G_KEY_LEFT 263
+#define G_KEY_DOWN 264
+#define G_KEY_UP 265
+#define G_KEY_PAGE_UP 266
+#define G_KEY_PAGE_DOWN 267
+#define G_KEY_HOME 268
+#define G_KEY_END 269
+#define G_KEY_CAPS_LOCK 280
+#define G_KEY_SCROLL_LOCK 281
+#define G_KEY_NUM_LOCK 282
+#define G_KEY_PRINT_SCREEN 283
+#define G_KEY_PAUSE 284
+#define G_KEY_F1 290
+#define G_KEY_F2 291
+#define G_KEY_F3 292
+#define G_KEY_F4 293
+#define G_KEY_F5 294
+#define G_KEY_F6 295
+#define G_KEY_F7 296
+#define G_KEY_F8 297
+#define G_KEY_F9 298
+#define G_KEY_F10 299
+#define G_KEY_F11 300
+#define G_KEY_F12 301
+#define G_KEY_F13 302
+#define G_KEY_F14 303
+#define G_KEY_F15 304
+#define G_KEY_F16 305
+#define G_KEY_F17 306
+#define G_KEY_F18 307
+#define G_KEY_F19 308
+#define G_KEY_F20 309
+#define G_KEY_F21 310
+#define G_KEY_F22 311
+#define G_KEY_F23 312
+#define G_KEY_F24 313
+#define G_KEY_F25 314
+#define G_KEY_TAB 258;
+#define G_KEY_CAPS 280;
+
+/* numpad keys */
+#define G_KEY_NP_0 320;
+#define G_KEY_NP_1 321;
+#define G_KEY_NP_2 322;
+#define G_KEY_NP_3 323;
+#define G_KEY_NP_4 324;
+#define G_KEY_NP_5 325;
+#define G_KEY_NP_6 326;
+#define G_KEY_NP_7 327;
+#define G_KEY_NP_8 328;
+#define G_KEY_NP_9 329;
+#define G_KEY_NP_DECIMAL 330
+#define G_KEY_NP_DIVIDE 331
+#define G_KEY_NP_MULTIPLY 332
+#define G_KEY_NP_SUBTRACT 333
+#define G_KEY_NP_ADD 334
+#define G_KEY_NP_ENTER 335
+#define G_KEY_NP_EQUAL 336
+
+/* modifier keys */
+#define G_KEY_LEFT_SHIFT 340
+#define G_KEY_LEFT_CONTROL 341
+#define G_KEY_LEFT_ALT 342
+#define G_KEY_LEFT_SUPER 343
+#define G_KEY_RIGHT_SHIFT 344
+#define G_KEY_RIGHT_CONTROL 345
+#define G_KEY_RIGHT_ALT 346
+#define G_KEY_RIGHT_SUPER 347
+#define G_KEY_MENU 348
 
 #endif /* UTILS_GKEYCODE_H_ */
