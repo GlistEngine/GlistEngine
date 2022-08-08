c0db372f9c625924f3d467fa86f4a4ab5a3c739f
knkar <knkar@Ali-Business>
2022-08-08 18:23:57 +0300

    more developed version

--- engine/ui/gGUINumberBox.h
+++ engine/ui/gGUINumberBox.h
@@ -1,25 +1,33 @@
 /*
  * gGUINumberBox.h
  *
- *  Created on: 27 Tem 2022
+ *  Created on: 27 Jul 2022
  *      Author: Aliv
  */
 
 #ifndef UI_GGUINUMBERBOX_H_
 #define UI_GGUINUMBERBOX_H_
 
-#include "gGUIControl.h"
+#include "gGUIContainer.h"
 
-class gGUINumberBox: public gGUIControl {
+class gGUINumberBox: public gGUIContainer {
 public:
 	gGUINumberBox();
 	virtual ~gGUINumberBox();
 
 	void draw();
+	/*
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	*/
 
 private:
 	int boxwidth, boxheight, numboxwidth, numboxheight, smalboxwidth, smalboxheight;
 
+	bool b1ispressed, b2ispressed;
+	bool b1istoggle, b2istoggle;
+	bool b1ispressednow, b2ispressednow;
+	bool b1isdisabled, b2isdisabled;
 };
 
 #endif /* UI_GGUINUMBERBOX_H_ */
