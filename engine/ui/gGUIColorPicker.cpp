e205fa93c578916f435a6279a72b2e9554a74046
Hp <Hp@LAPTOP-MOTBQD0H>
2022-08-18 10:10:59 +0300

    colorpicker'a siyah ve beyaz seçeneği eklendi

---
+++ engine/ui/gGUIColorPicker.cpp
@@ -0,0 +1,76 @@
+/*
+ * gGUIColorPicker.cpp
+ *
+ *  Created on: 8 Aðu 2022
+ *      Author: Hp
+ */
+
+#include "gGUIColorPicker.h"
+
+gGUIColorPicker::gGUIColorPicker() {
+	lastcolorforline = false;
+	isdisabled = false;
+	ispressed = false;
+	colorpicker.loadImage("ColorPicker2.png");
+	colorpickerw = colorpicker.getWidth();
+	colorpickerh = colorpicker.getHeight();
+	columnnum = 1;
+	data = colorpicker.getImageData();
+}
+
+gGUIColorPicker::~gGUIColorPicker() {
+
+}
+void gGUIColorPicker::draw() {
+	colorpicker.draw(left, top);
+	if(ispressed) {
+		gColor oldcolor = renderer->getColor();
+		renderer->setColor(0, 0, 0, 255);
+		gDrawRectangle(cx, cy, 16, 16, false);
+		renderer->setColor(oldcolor);
+	}
+}
+
+void gGUIColorPicker::mousePressed(int x, int y, int button) {
+	if(isdisabled) return;
+	if(x >= left && x < left + colorpicker.getWidth() && y >= top && y < top + colorpicker.getHeight() ) {
+		if(x - left > 0 && x - left <= 227 && y - top > 0 && y - top <= 227) {
+			ispressed = true;
+			cx = (x - left) / 16;
+			column = cx + 1; // 1-14
+			cx = cx * 16 + left + 3;
+			cy = (y - top) / 16;
+			line = cy + 1; // 1-14
+			cy = cy * 16 + top + 1;
+			if(cx > 212) cx = 214;
+			if(cy > 230) cy = 241;
+		//	gLogi("GameCanvas") << "mousePressed" << ", cx:" << cx << ", cy:" << cy;
+			//gLogi("GameCanvas") << "mousePressed" << ", column:" << column << ", line:" << line;
+			countColors(column, line);
+		}
+		if(x - left > 0 && x - left <= 227 && y - top > 254 && y - top <= colorpicker.getHeight()) {
+			ispressed = true;
+			cx = (x - left) / 16;
+			column = cx + 1;
+			cx = cx * 16 + left + 3;
+			cy = 283;
+			line = 15;
+			gLogi("GameCanvas") << "mousePressed" << ", cx:" << column << ", cy:" << line;
+			gLogi("GameCanvas") << "mousePressed" << ", cx:" << cx << ", cy:" << cy;
+			countColors(column, line);
+		}
+		root->getCurrentCanvas()->onGuiEvent(id, G_GUIEVENT_COLORSELECTED);
+	}
+}
+
+void gGUIColorPicker::countColors(int column, int line) {
+	startcolorpixel = colorpicker.getWidth() * 16 * (line) * 4 + 32 + 64 * (column - 1);
+	if(line > 14) startcolorpixel = colorpicker.getWidth() * 16 * (line + 1) * 4 + 32 + 64 * (column - 1);
+	lastdata = startcolorpixel + 4; // 227-269
+		for(int i = startcolorpixel; i <= lastdata; i++) {
+			gLogi("GameCanvas") << "----" << ", column:" << column << ", line:" << line;
+			gLogi("--------") << " R " << (short)data[i] << " G " << (short)data[i + 1] << " B "<< (short)data[i + 2] << " A "<< (short)data[i + 3];
+			i = i + 3; //next pixel data (i + 4)
+			i = i + 60; // next color data (i + 64)
+	}
+}
---
+++ engine/ui/gGUIColorPicker.h
@@ -0,0 +1,41 @@
+/*
+ * gGUIColorPicker.h
+ *
+ *  Created on: 8 Aðu 2022
+ *      Author: Hp
+ */
+
+#ifndef UI_GGUICOLORPICKER_H_
+#define UI_GGUICOLORPICKER_H_
+
+#include "gGUIImageButton.h"
+#include "gImage.h"
+#include "gBaseCanvas.h"
+
+class gGUIColorPicker: public gGUIControl {
+public:
+	gGUIColorPicker();
+	virtual ~gGUIColorPicker();
+
+	void draw();
+	void mousePressed(int x, int y, int button);
+	void countColors(int column, int line);
+
+private:
+	int cx, cy;
+	int colorpickerw, colorpickerh;
+	int linenum;
+	int pixel1, pixel2;
+	int startcolorpixel;
+	int lastdata;
+	int columnnum;
+	int column, line;
+	bool lastcolorforline;
+	bool ispressed;
+	bool isdisabled;
+
+	gImage colorpicker;
+	unsigned char *data;
+};
+
+#endif /* UI_GGUICOLORPICKER_H_ */
