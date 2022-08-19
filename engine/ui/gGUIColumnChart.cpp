ad63ff1bea071e60e8f1867ec460d9166bfdfe6e
ncrx7 <buysal31@gmail.com>
2022-08-19 19:18:44 +0300

    dfdfdf

--- engine/ui/gGUIColumnChart.cpp
+++ engine/ui/gGUIColumnChart.cpp
@@ -18,7 +18,7 @@ gGUIColumnChart::gGUIColumnChart() {
 	indexcolumn = 0;
 	indexmulticolumn = 0;
 	initialmcspadding = 5;
-	columnsw = 15;
+	columnsw = 0;
 	chartaspect = 0;
 	chartliney = 0;
 	numberrange = 0;
@@ -27,6 +27,9 @@ gGUIColumnChart::gGUIColumnChart() {
 	numbersinchart = "";
 	colorindex = 0;
 	columnnumber = 0;
+	gridlineison = 1;
+	numbersison = 1;
+	textison = 1;
 }
 
 gGUIColumnChart::~gGUIColumnChart() {
@@ -41,8 +44,6 @@ void gGUIColumnChart::draw() {
 	gDrawLine(left+ chartx , top + charty, left + chartx + chartw, top + charty); //horizontal
 	drawColumns();
 	drawMultiColumns();
-	//drawLines();
-	//gLogi("paramters") << multicolumnh[1][1];
 	renderer->setColor(oldcolor);
 
 }
@@ -51,7 +52,7 @@ void gGUIColumnChart::mouseMoved(int x, int y) {
 
 }
 
-void gGUIColumnChart::setChartProp(int chartx, int charty, int chartw, int charth, int chartnumberrange) {
+void gGUIColumnChart::setChartProp(int chartx, int charty, int chartw, int charth,int columnsw, int columnsmargin, int chartnumberrange) {
 	this->chartx =  chartx;
 	this->charty = charty;
 	this->chartw = chartw;
@@ -61,6 +62,8 @@ void gGUIColumnChart::setChartProp(int chartx, int charty, int chartw, int chart
 	this->multicolumnx = chartx + 20;
 	this->multicolumny = charty;
 	this->numberrange = chartnumberrange;
+	this->columnsw = columnsw;
+	this->columnsmargin = columnsmargin;
 }
 
 void gGUIColumnChart::addColumn(int columnh, gColor color) {
@@ -71,9 +74,7 @@ void gGUIColumnChart::addColumn(int columnh, gColor color) {
 	singlecolumscolor.push_back(colors);
 	columnhe.push_back(columnh);
 	this->columnh = columnh;
-	//gDrawRectangle(left + columnx + 5, top + columny - columnh, 15, columnh, true);
-	//gDrawRectangle(left + columnx + 5, top + columny - columnh, 15, columnh, true);
-	columnx += 25;
+	columnx += columnsmargin / 2 + 5;
 	columnxe.push_back(columnx);
 	indexcolumn += 1;
 
@@ -88,7 +89,9 @@ void gGUIColumnChart::drawColumns() {
 	}
 }
 
-void gGUIColumnChart::addMultiColumn(int columnnum,int columnh, int columnh2, int columnh3, int columnh4, gColor color, gColor color2, gColor color3, gColor color4, std::string text) {
+void gGUIColumnChart::addMultiColumn(int columnnum,int columnh, int columnh2, int columnh3, int columnh4, gColor color, gColor color2, gColor color3, gColor color4, std::string text, int multicolumnspadding) {
+	this->multicolumnspadding = multicolumnspadding;
+	multicolumnspaddingvector.push_back(multicolumnspadding);
 	columnnumber = columnnum;
 	std::vector<int> colors;
 	textvector.push_back(text);
@@ -113,10 +116,10 @@ void gGUIColumnChart::addMultiColumn(int columnnum,int columnh, int columnh2, in
 	multicolumnh.push_back(newmulticolumnh);
 	multicolumnxe.push_back(multicolumnx);
 	//multicolumnx += (columnh + columnh2 + columnh3 + columnh4) / 4;
-	if(columnnum == 1) multicolumnx += columnsw + 20;
-	else if(columnnum == 2) multicolumnx += columnsw * 2 + ((columnnum - 1) * (initialmcspadding)) + 35;     //55
-	else if(columnnum == 3) multicolumnx += columnsw * 3 + ((columnnum - 1) * (initialmcspadding)) + 35;     //90
-	else if(columnnum == 4) multicolumnx += columnsw * 4 + ((columnnum - 1) * (initialmcspadding)) + 35;    //130
+	if(columnnum == 1) multicolumnx += columnsw + columnsmargin;
+	else if(columnnum == 2) multicolumnx += columnsw * columnnum + ((columnnum - 1) * (initialmcspadding)) + columnsmargin;     //55
+	else if(columnnum == 3) multicolumnx += columnsw * columnnum + ((columnnum - 1) * (initialmcspadding)) + columnsmargin;     //90
+	else if(columnnum == 4) multicolumnx += columnsw * columnnum + ((columnnum - 1) * (initialmcspadding)) + columnsmargin;    //130
 	//multicolumnx += 130;
 
 
@@ -128,14 +131,16 @@ void gGUIColumnChart::drawMultiColumns() {
 	for(int i = 0; i < indexmulticolumn; i++) {
 		gColor oldcolor = renderer->getColor();
 		renderer->setColor(middlegroundcolor);
-		this->title = textvector[i];
-		font->drawText(title, left + multicolumnxe[i] + ((columnsw * columnnumber + ((columnnumber - 1) * initialmcspadding ) - font->getStringWidth(title)) / 2), top + charty + 15);
+		if(textison) {
+			this->title = textvector[i];
+			font->drawText(title, left + multicolumnxe[i] + ((columnsw * columnnumber + ((columnnumber - 1) * initialmcspadding ) - font->getStringWidth(title)) / 2), top + charty + 15); //columnnumberý vectore dönüþtür
+		}
 		renderer->setColor(oldcolor);
 		colorindex = 0;
 		for(int j = 0; j < 4; j++) {
 			renderer->setColor(multicolumscolor[i][colorindex], multicolumscolor[i][colorindex + 1], multicolumscolor[i][colorindex + 2]);
 			gDrawRectangle(left + initialmcspadding + multicolumnxe[i] , top + columny - multicolumnh[i][j], columnsw, multicolumnh[i][j], true );
-			initialmcspadding += 20;
+			initialmcspadding += multicolumnspaddingvector[i];
 			colorindex +=3;
 		}
 		renderer->setColor(oldcolor);
@@ -143,34 +148,38 @@ void gGUIColumnChart::drawMultiColumns() {
 	}
 }
 
-
-/*void gGUIColumnChart::drawLines() {
-	for(int i = 0; i < indexmulticolumn; i++) {
-		for(int j = 0; j < 4; j++) {
-			gColor oldcolor = renderer->getColor();
-			renderer->setColor(middlegroundcolor);
-			gDrawLine(left+ chartx , top + charty - multicolumnh[i][j], left + chartx + chartw, top + charty - multicolumnh[i][j]);
-			renderer->setColor(oldcolor);
-		}
-	}
-}*/
-
 void gGUIColumnChart::drawLinesAndNumbers() {
 	numbersinchart = "";
 	numberrangey = 0;
 	chartliney = 0;
 	chartaspect = charth / 10;
 	numberrangeaspect = numberrange / 10;
-	for(int i = 0; i < (charth / chartaspect) - 1; i++) {
-		chartliney += chartaspect;
-		numberrangey += numberrangeaspect;
-		numbersinchart = gToStr(numberrangey);
-		gColor oldcolor = renderer->getColor();
-		renderer->setColor(middlegroundcolor);
-		gDrawLine(left+ chartx , top + charty - chartliney, left + chartx + chartw, top + charty - chartliney);
-		this->title = numbersinchart;
-		font->drawText(title, left + chartx - font->getStringWidth(title) - 8, top + charty - chartliney);
-		renderer->setColor(oldcolor);
-		//gLogi("chartliney : ") << chartliney;
+	if(gridlineison) { for(int i = 0; i < (charth / chartaspect) - 1; i++) {
+			chartliney += chartaspect;
+			gColor oldcolor = renderer->getColor();
+			renderer->setColor(middlegroundcolor);
+			gDrawLine(left+ chartx , top + charty - chartliney, left + chartx + chartw, top + charty - chartliney);
+			renderer->setColor(oldcolor);
+			//gLogi("chartliney : ") << chartliney;
+		}
 	}
+	chartliney = 0;
+	if(numbersison)	{ for(int i = 0; i < (charth / chartaspect) - 1; i++) {
+			chartliney += chartaspect;
+			numberrangey += numberrangeaspect;
+			numbersinchart = gToStr(numberrangey);
+			gColor oldcolor = renderer->getColor();
+			renderer->setColor(middlegroundcolor);
+			this->title = numbersinchart;
+			font->drawText(title, left + chartx - font->getStringWidth(title) - 8, top + charty - chartliney);
+			renderer->setColor(oldcolor);
+			//gLogi("chartliney : ") << chartliney;
+		}
+	}
+}
+
+void gGUIColumnChart::setVisibility(bool gridlineison, bool numbersison, bool textison) { // If this function doesn't use, boolean variables will be accepted as TRUE
+	this->gridlineison = gridlineison;
+	this->numbersison = numbersison;
+	this->textison = textison;
 }
--- engine/ui/gGUIColumnChart.h
+++ engine/ui/gGUIColumnChart.h
@@ -1,8 +1,36 @@
 /*
  * gGUIColumnChart.h
  *
- *  Created on: 11 A�u 2022
- *      Author: gsb4t
+ *  Created on: 11 Ağu 2022
+ *      Author: Batuhan
+ *-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ *                                         HOW TO USE
+ *    First, create an object of this class on the canvas.
+ *    Then call the class's setChartProp method using this object.
+ *    Enter the expected values ​​respectively as parameters of this method. Parameter description below.
+ *     -chartx and charty variables set position of x and y axis.
+ *     -chartw and charth variables set width and height of x and y axis.
+ *     -columnsw is width of the columns (both normal column and multi columns)
+ *     -columnsmargin is distance of between columns.(is the distance between multi columns and other multi columns for multicolumns)
+ *     -chartnumberrange is the value range on on the y-axis.The value you enter will be printed as equal divisions from 0 to the number you enter.
+ *    After that you can add single columns by using addColumn function. Parameter description below.
+ *     -columnh is the height of the column which you add.
+ *     -color is the color of the column. (Example usage on the canvas : , gColor(100, 100, 100) )
+ *    If you want to create multicolumn group, you can use addMultiColumn function. Parameter description below.
+ *     -(ATTENTION) columnnum is how many column that you want to add to group (max limit 4 column for now).
+ *     	 This parameter provides that x location of last columns which you specified in multicolum group is calculated automatically.
+ *     	 So, even If you are going to enter a value of 0 to your column, please enter to the columnum parameter by counting the column which you value 0.
+ *     	 If you want to add fewer than 4 column, after you write columnum, you have to write 0 again other columnh which you don't want to add.
+ *     	 ***thus, the width of the column which you don't want to add is not calculated for margin.***
+ *     -columnh ,columnh2, columnh3, columnh4 are the heights of the columns in the group.
+ *     -The next 4 color parameters specify the colors of the columns, respectively.
+ *       Even if you are going to create less than 4 columns, enter a gColor value for these 4 parameters.
+ *      -title parameter is what typing under multi columns group.
+ *      -multicolumnspadding is a parameter where you specify how much space you want between multi columns group.
+ *    Finally, you can set visibility of additions. (Ex : visibility of grid lines, numbers on y-axis or text under x-axis).
+ *    	-Parameters are girlineison, numberison, textison. (Event you don't use this method, values ​​will be accepted TRUE).
+ *
+ *
  */
 
 #ifndef UI_GGUICOLUMNCHART_H_
@@ -17,10 +45,10 @@ public:
 
 	void draw();
 	void mouseMoved(int x, int y);
-	void setColumn();
-	void setChartProp(int chartx, int charty, int chartw, int charth, int chartnumberrange); //number range : 0 - chartnumberrange
+	void setChartProp(int chartx, int charty, int chartw, int charth, int columnsw, int columnsmargin, int chartnumberrange); //number range : 0 - chartnumberrange
 	void addColumn(int columnh, gColor color);
-	void addMultiColumn(int columnum = 1, int columnh = 0, int columnh2 = 0, int columnh3 = 0, int columnh4 = 0, gColor color = {100, 100, 100}, gColor color2 = {100, 100, 100}, gColor color3 = {100, 100, 100}, gColor color4 = {100, 100, 100}, std::string title = "");
+	void addMultiColumn(int columnum = 1, int columnh = 0, int columnh2 = 0, int columnh3 = 0, int columnh4 = 0, gColor color = {100, 100, 100}, gColor color2 = {100, 100, 100}, gColor color3 = {100, 100, 100}, gColor color4 = {100, 100, 100}, std::string title = "", int multicolumnspadding = 0);
+	void setVisibility(bool gridlineison = 1, bool numbersison = 1, bool textison = 1);
 private:
 	int chartw, charth;
 	int chartx, charty;
@@ -38,6 +66,9 @@ private:
 	int numberrangeaspect;
 	int colorindex;
 	int columnnumber;
+	int columnsmargin;
+	int multicolumnspadding;
+	bool gridlineison, numbersison, textison;
 	std::string numbersinchart;
 	std::vector<int> columnhe;
 	std::vector<int> columnxe;
@@ -46,6 +77,7 @@ private:
 	std::vector<std::vector<int>> singlecolumscolor;
 	std::vector<std::vector<int>> multicolumscolor;
 	std::vector<std::string> textvector;
+	std::vector<int> multicolumnspaddingvector;
 
 	void drawColumns();
 	void drawMultiColumns();
