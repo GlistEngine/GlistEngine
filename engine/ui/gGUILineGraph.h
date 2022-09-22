75c7c4aeb0c14120923d4f6bc5e46b82a5bf1816
R.Noyan Culum <noyanculum@yahoo.com>
2022-09-23 01:01:27 +0300

    gGUIGraph base class implemented, gGUILineGraph extended from gGUIGraph

--- engine/ui/gGUILineGraph.h
+++ engine/ui/gGUILineGraph.h
@@ -14,12 +14,12 @@
 #include "gGUIPanel.h"
 #include "gGUIButton.h"
 #include "gGUITextbox.h"
-#include "gGUIControl.h"
+#include "gGUIGraph.h"
 #include "gLine.h"
 #include "gRectangle.h"
 #include "gCircle.h"
 
-class gGUILineGraph: public gGUIControl  {
+class gGUILineGraph: public gGUIGraph  {
 
 public:
 	gGUILineGraph();
@@ -28,49 +28,23 @@ public:
 	void set(gBaseApp* root, gBaseGUIObject* topParentGUIObject, gBaseGUIObject* parentGUIObject, int parentSlotLineNo, int parentSlotColumnNo, int x, int y, int w, int h);
 
 	void setMaxX(int maxX);
-	int getMaxX();
 	void setMinX(int minX);
-	int getMinX();
 	void setMaxY(int maxY);
-	int getMaxY();
 	void setMinY(int minY);
-	int getMinY();
-	void setGridlinesX(bool gridlinesX);
-	void setGridlinesY(bool gridlinesY);
-	void setTitleX(std::string titleX);
-	std::string getTitleX();
-	void setTitleY(std::string titleY);
-	std::string getTitleY();
+
 	void setLabelCountX(int labelCount);
-	int getLabelCountX();
 	void setLabelCountY(int labelCount);
-	int getLabelCountY();
 
 	void addLine();
 	void addPointToLine(int lineindex, float x, float y);
 
-	void draw();
-
 
 private:
 	void drawGraph();
-	void drawLabels();
-	void drawLines();
 	void updatePoints();
-	void updateLabelsX();
-	void updateLabelsY();
 
-	float axisx1, axisy1, axisx2, axisy2;
-	float axisxw, axisyh;
-	float maxy, miny, maxx, minx;
 	std::vector<std::vector<std::array<float, 4>>> graphlines;
-	float labelwidthx, labelwidthy;
-	bool drawgridlinesx, drawgridlinesy;
-	std::string axisytitle, axisxtitle;
 	gColor linecolors[5];
-	int labelcountx, labelcounty;
-	std::vector<int> labelsx;
-	std::vector<int> labelsy;
 };
 
 
