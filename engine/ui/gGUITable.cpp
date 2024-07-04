f14f2b718b2e7fdb8a49f72719b59a2914743edc
YousifAljbouri <154931090+UsifDev@users.noreply.github.com>
2024-07-04 17:25:54 +0300

    (new function) dynamically add folder items to table
    added a new functionality to iterate over items in a given full folder path and then adding the appropriate image to the item.
    
    variables added:
    1/ a new collection of image pointers (dirimages) specifically for the folder images to persist them as long as the table is alive and deleting them upon deconstruction.
    This is to prevent accidentally deleting resource icons when using the imagelist collection.
    2/ private gGUIResource variable and initialize it in constructor.
    
    addFolderItems method flow:
    loop item over directory:
    if item is folder then additem(folder icon, file name);
    push new image into dirimages and attempt load;
    if item is file and not image file then pop the image and additem(file icon, file name);
    if item is image file then additem(img, file name);
    
    example client code:
    table.addFolderItems(gGetImagesDir());

--- engine/ui/gGUITable.cpp
+++ engine/ui/gGUITable.cpp
@@ -30,7 +30,7 @@ gGUITable::gGUITable() {
 }
 
 gGUITable::~gGUITable() {
-
+	for(auto img: dirimages) delete img;
 }
 
 void gGUITable::set(gBaseApp *root, gBaseGUIObject *topParentGUIObject,
@@ -121,6 +121,35 @@ void gGUITable::draw() {
 void gGUITable::addItem(gTexture* image, std::string title) {
 	imagelist.push_back(image);
 	imagetextlist.push_back(title);
+}
+
+/*
+ * iterates over elements in a folder, adding them to the gGUITable object.
+ * Checks first if the element is a file or folder, then if the file is an image file.
+ * if the element is file or folder it adds the respective icon from gGUIResources and if its an image file it uses it instead.
+ * the algorithm creates images using pointers, so it must be deleted in the class deconstructer.
+ */
+void gGUITable::addFolderItems(const std::string& fullfolderpath){
+	//create fs path to use directory_iterator
+	const std::__fs::filesystem::path dir{fullfolderpath};
+	for(auto const& dir_entry : std::__fs::filesystem::directory_iterator{dir}){
+		std::string path = dir_entry.path().generic_string();
+		//check if its a folder or a file else continue to next entry
+		if(gFile::isDirectory(path)){
+			addItem(res.getIconImage(gGUIResources::ICON_FOLDER), gFile::getFilename(path));
+		} else if(gFile::isFile(path)){
+			// create a new image, load it with the file then verify if the file is really an image
+			dirimages.push_back(new gImage());
+			auto img = dirimages.back();
+			img->load(path); // try loading the file as an image; the file is not an image if either height or width is 0
+			if(img->getHeight() == 0 || img->getWidth() == 0){
+				dirimages.pop_back(); // get rid of the invalid image
+				addItem(res.getIconImage(gGUIResources::ICON_FILE), gFile::getFilename(path));
+			}else{
+				addItem(img, gFile::getFilename(path));
+			}
+		}
+	}
 }
 
 void gGUITable::setFontColor(gColor color) {
