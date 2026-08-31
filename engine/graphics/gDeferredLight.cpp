0f8a7e84b4fa9434c47e872a73370b5e6bc01cc2
irrld <irrelevantdev@outlook.com>
2026-08-31 16:55:50 +0300

    Include GL through gRenderer in gDeferredLight

--- engine/graphics/gDeferredLight.cpp
+++ engine/graphics/gDeferredLight.cpp
@@ -1,7 +1,6 @@
 #include "gDeferredLight.h"
 #include "gRenderer.h"
-#include <GL/glew.h>
-#include<iostream>
+#include <iostream>
 #include "gUtils.h"
 #include "graphics/shaders/deferred_geom_vert.h"
 #include "graphics/shaders/deferred_geom_frag.h"
